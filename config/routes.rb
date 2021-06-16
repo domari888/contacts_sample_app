278fd9d63415a28a6c67ac3d0b95f75b6659a2a3
damari <motokin128@gmail.com>
2021-06-16 18:12:00 +0900

    お問い合わせフォームを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root "homes#index"
+  resources :contacts, only: %i[index creae]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
